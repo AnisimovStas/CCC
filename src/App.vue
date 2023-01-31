9e2a9f47c6b33e9a37c7e2434153d281e848859d
Feralbot <staaasyyaaan@gmail.com>
2023-01-31 09:27:51 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="container bg-slate-300">
+    <header class="bg-red-500 flex flex-col text-white">
+      header
+      <label>currency 1 </label><input /> <label>currency 2 </label><input />
+      <button>add</button>
+    </header>
+    <main class="container">
+      <div class="flex">
+        <div class="flex flex-row space-x-2">
+          <div>dataName1</div>
+          <div>dataName1</div>
+          <div>DataPrice</div>
+          <button>delete</button>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
